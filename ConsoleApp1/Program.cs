82f23e84b5e37bd286adecab6a868b6e16432c83
sikle00 <92247756+sikle00@users.noreply.github.com>
2024-06-19 13:44:32 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,106 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public void Shoplist()
+        {
+            var shop1 = new Shop("Coop", "food", 2);
+            var shop2 = new Shop("Netonet", "tech", 3);
+            var shop3 = new Shop("Nille", "allrounder", 1);
+            List<Shop> stores = new List<Shop>();
+            stores.Add(shop1);
+            stores.Add(shop2);
+            stores.Add(shop3);
+        }
+        static void Main(string[] args)
+        {
+            var shop1 = new Shop("Coop", "food", 2);
+            var shop2 = new Shop("Netonet", "tech", 3);
+            var shop3 = new Shop("Nille", "allrounder", 1);
+            List<Shop> stores = new List<Shop>();
+            stores.Add(shop1);
+            stores.Add(shop2);
+            stores.Add(shop3);
+            while (true)
+            {
+                Console.Clear();
+                showMenu();
+                Console.ReadKey();
+            }
+            void showMenu()
+            {
+                Console.WriteLine("For overview over all stores, press 1");
+                Console.WriteLine("For all cheap stores, press 2");
+                Console.WriteLine("For all middle class stores, press 3");
+                Console.WriteLine("For all expensive stores, press 4");
+                Console.WriteLine("To add a new store, press 5");
+                Console.WriteLine("To exit the application, press 6");
+                var input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        ShowOverview();
+                        break;
+                    case "2":
+                        ShowPrice(1);
+                        break;
+                    case "3":
+                        ShowPrice(2);
+                        break;
+                    case "4":
+                        ShowPrice(3);
+                        break;
+                    case "5":
+                        AddShop();
+                        break;
+                    case "6":
+                        Console.WriteLine("Until next time!");
+                        System.Environment.Exit(0);
+                        break;
+                }
+            }
+            void ShowOverview()
+            {
+                foreach (Shop shop in stores)
+                {
+                    shop.show();
+                }
+            }
+            void ShowPrice(int priceLevel)
+            {
+                /*for (int i = 0; i < stores.Count; i++) { 
+                if (Shop.priceLevel == "1")
+                {
+                    for (int i = 0; i < Shop.Count; i++)
+                    {
+                        Console.WriteLine(Shop[i].Navn);
+                    }
+                    }
+                }*/
+                foreach (Shop shop in stores)
+                {
+                    if (shop.PriceLevel == priceLevel)
+                    {
+                        shop.show();
+                    }
+                }
+            }
+
+
+            void AddShop()
+            {
+                Console.WriteLine("Please assign a name for the shop");
+                string Shopname = Console.ReadLine();
+                Console.WriteLine("Please assign a speciality for the shop");
+                string Shopspecification = Console.ReadLine();
+                Console.WriteLine("Please assign a priceclass for the shop");
+                int ShopPriceClass = int.Parse(Console.ReadLine());
+                var shop = new Shop(Shopname, Shopspecification, ShopPriceClass);
+                Console.WriteLine($"You have added {Shopname} in stores");
+                stores.Add(shop);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Shop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Shop
+    {
+        public string Name { get; set; }
+        public string Speciality { get; set; }
+        public int PriceLevel { get; set; }
+        public Shop(string name, string speciality, int priceLevel)
+        {
+            PriceLevel = priceLevel;
+            Name = name;
+            Speciality = speciality;
+        }
+        public class Stores
+        {
+            List<Shop> stores = new List<Shop>();
+        }
+        public void show()
+        {
+            Console.WriteLine($"Store name: {Name}, Speciality: {Speciality}, Priceclass: {PriceLevel}");
+        }
+    }
+}
